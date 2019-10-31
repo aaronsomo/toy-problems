d35f378a664d925a9b50ccffdbc299f5e3e47dc6
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2019-10-31 09:57:07 -0700

    Progress towards solving hashTableResizing.js

--- hashTableResizing/hashTableResizing.js
+++ hashTableResizing/hashTableResizing.js
@@ -25,20 +25,100 @@ var makeHashTable = function() {
   var storage = [];
   var storageLimit = 4;
   var size = 0;
+
+  if (size > storage.length) {
+    storageLimit = storageLimit * 2;
+    for (var i = 0; i < storage.length; i++) {
+    this.insert(storage[i][0], storage[i][1]); // throwing errors in console, out of time
+    }
+  }
   
-  result.insert = function(/*...*/ 
+  result.insert = function(k, v 
 ) {
     // TODO: implement `insert`
+    /*
+    things to keep in mind: when inserting, make sure to check and see if key already exists, replace value
+
+    store index in variable
+    need to create bucket for tuples
+    if bucket does not exist 
+      create bucket 
+        iterate over bucket 
+        create tuple 
+          if tuple[0] is equal to key 
+            set tuple[1] as value
+            push tuple to the bucket
+          if key already exists
+            replace existing value with new value
+    */
+    var index = getIndexBelowMaxForKey(k, storageLimit);
+    var bucket = storage[index];
+    
+    if (!bucket) {
+      bucket = [];
+      storage[index] = bucket;
+    }
+    
+    var keyExists = false;
+    
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      if (tuple[0] === key) {
+        tuple[1] = v;
+        keyExists = true;
+      }
+    }
+    
+    if(!keyExists) {
+      bucket.push([k, v]);
+      size++; // increase size
+    }
   };
 
-  result.retrieve = function(/*...*/ 
+  result.retrieve = function(k 
 ) {
     // TODO: implement `retrieve`
+    /*
+    store index in variable
+    create bucket with storage[index]
+    iterate over bucket
+        if tuple[0] === key
+          return tuple[1]
+    if bucket does not exist 
+    return null
+    */
+   var index = getIndexBelowMaxForKey(k, storageLimit);
+   var bucket = storage[index];
+   if (bucket.length > 0) {
+    for (var i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === k) {
+        return bucket[i][1];
+      }
+    }
+  }
+  return null;
   };
 
-  result.remove = function(/*...*/ 
+  result.remove = function(k 
 ) {
     // TODO: implement `remove`
+    /*
+    store index in variable
+    create bucket with storage[index]
+    iterate over bucket
+      if(tuple[0] === key) 
+        bucket.splice i, 1 
+      return tuple[1]
+    */
+   var index = getIndexBelowMaxForKey(k, storageLimit);
+
+   var bucket = storage[index];
+   for (var i = 0; i < bucket.length; i++) {    
+    if (bucket[i][0] === key) {
+      bucket.splice(i, 1);
+    }
+    return bucket[i][1];
+    }
   };
 
   return result;
