83c265c6a675c43f900131fa5d3b60b0d6a8b181
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2019-11-14 10:01:33 -0800

    Progress towards solving allAnagrams.js

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,35 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
+/*
+things to keep in mind: output all of the different combinations of strings when given an input string
+strings do not have to be real words
+strings can be repeated
+
+create a storage array
+practice and attempt solution with helper function/recursion
+  iterate over the input string
+    grab current letter
+    substring the rest of the input
+    grab another letter
+    possibly recurse over leftover letters
+      return concat
+
+*/
 var allAnagrams = function(string) {
   // Your code here.
+  var combinations = [];
+  if (string.length < 1) {
+    return;
+  }
+  for (var i = 0; i < string.length; i++) {
+    var letter = string[i];
+    var partialString = string.substring(0, i);
+    // var partialString = string.substring(i + 1, )
+    var possibleCombo = allAnagrams(partialString);
+    for (var j = 0; j < partialString.length; j++) {
+      combinations.push(letter + possibleCombo[j]);
+    }
+  }
+  return combinations;
 };
