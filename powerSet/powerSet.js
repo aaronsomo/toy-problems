f447927c4b9a0d50da7d1e5add8d1e4f4f58e665
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2019-11-16 09:58:41 -0800

    Progress towards solving powerSet.js

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -17,5 +17,52 @@
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
+/*
+things to keep in mind: combinations cannot be repeated, regardless of the order of characters
+
+I: 'abc'
+O: [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
+C:
+E: cannot have repeat combinations
+
+create a storage array
+we are going to recurse, so most likely create a helper function
+iterate over the input string
+  at each index, concat the current value into the result array
+    ["", "a", "ab", "abc"]
+  recurse helper function, slice the first index, so str = 'bc'
+    iterate through 'bc'
+      at each index, concat the current value into the result array
+        ["", "a", "ab", "abc", "b", "bc"]
+      recurse again, str = 'c'
+        iterate
+          ["", "a", "ab", "abc", "b", "bc", "c"]
+  return array
+
+*/
+
 var powerSet = function(str) {
+  var resultArray = [];
+  
+  function addToResult(str) {
+    if(str.length === 0){
+      return;
+    }
+    // if(resultArray.length > str.length){
+    //   return;
+    // }
+    
+    for (var i = 0; i < str.length; i++) {
+      resultArray.push(str[i]);
+      console.log(resultArray);
+      // addToResult(str.slice(i,1));
+    }
+    addToResult(str.slice(0, 1));
+  }
+  resultArray.unshift("");
+
+  addToResult(str);
+  return resultArray;
 };
+
+powerSet("jump");
