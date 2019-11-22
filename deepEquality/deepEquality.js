e464cabf87ae21e9a0f8cd16eabe9fd040ad4f8d
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2019-11-22 09:59:56 -0800

    Progress toward solving deepEquality.js

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,43 @@
   * don't worry about handling cyclical object structures.
   *
   */
+
+/*
+things to keep in mind: check if everything, including structure, between two objects are the same
+
+I:  deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})
+    deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})
+O:  true
+    false
+C:  none
+E:  if object is empty
+
+iterate through first object
+compare the keys and values of the first object with the second object
+if value is nested
+  recurse comparison
+
+*/
 var deepEquals = function(apple, orange) {
+
+  if (typeof apple == "object" && typeof orange == "object") {
+    if (apple === orange) {
+      return true
+    }
+    if (Object.keys(apple).length === Object.keys(orange).length) {
+
+      for (var key in apple) {
+        if (orange.hasOwnProperty(key)) {
+          deepEquals(apple[key], orange[key])
+        }
+      }
+
+    } else {
+      return false
+    }
+    return true
+  } else {
+    return false
+  }
+
 };
