8986b16be22a44557a965059b77acdb16a4eb950
Aaron Southammavong <aaroneight07@gmail.com>
2019-12-20 19:24:22 -0800

    Progress towards solving toy problem

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -1,17 +1,61 @@
 /**
- * Write a function that takes a number as its argument and 
+ * Write a function that takes a number as its argument and
  * returns a string that represents that number's simplified fraction.
  *
  * Example: toFraction(0.5) === '1/2'
- * 
- * Whole numbers and mixed fractions should be returned as irregular fractions
- * 
- * Example: toFraction(3.0) === '3/1'
- * 
- * Example: toFraction(2.5) === '5/2'
  *
+ * Whole numbers and mixed fractions should be returned as irregular fractions
+ *
+ * Example: toFraction(3.0) === '3/1'
+ *
+ * Example: toFraction(2.5) === '5/2'
+ */
+
+/*
+  things to keep in mind: whole numbers and mixed fractions should be returned as irregular fractions
+
+  I: 2.5
+  O: 5/2
+  C:
+  E: if input number = 0, return 0
+
+  multiply number by orders of 10 until modulus isn't 0 to get a whole number
+  create variable to store denominator that keeps track of multiples of 10
+
+  iterate decrementally from current `number` value and modulo `i` until 0
+  check to see if `denominator` modulo `i` is 0
+    if true, we found the greatest common denominator
+
+  return STRING (number/greatest common denominator) + "/" + (denominator/greatest common denominator)
  */
 
 var toFraction = function(number) {
   // Your code here
+  var denominator = 1;
+  var greatestCommonDenominator = 1;
+
+  if (number === 0) {
+    return 0;
+  }
+
+  while (num % 1 !== 0) {
+    number = number * 10;
+    denominator = denominator * 10;
+  }
+
+  for (var i = number; i > 0; i--) {
+    if (number % i === 0) {
+      if (denominator % i === 0) {
+        greatestCommonDenominator = i;
+        break; // callstack if no break
+      }
+    }
+  }
+
+  // we want to return a STRING in the form of a fraction/irregular fraction
+  return (
+    number / greatestCommonDenominator +
+    "/" +
+    denominator / greatestCommonDenominator
+  );
 };
--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -7,8 +7,30 @@
 * - DO NOT CONVERT THE INPUT INTO A STRING OR ARRAY. 
 * - Only use integers and math in your solution.
 *
+things to keep in mind: reverse the order of the numbers
+
+I: 123
+O: 321
+C:
+E: if input is negative, return null. if input is 0, return 0.
+
+create reverse number variable
+find a way to isolate each number
+  do not convert input into an array
+    use math to isolate individual numbers
+
 */
 
-function reverseInteger(number){
+function reverseInteger(number) {
   // TODO: Implement this function!
+
+  if (number < 0) {
+    return null;
+  }
+
+  if (number === 0) {
+    return 0;
+  }
+
+  var reverse = number;
 }
