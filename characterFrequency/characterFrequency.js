d3d3d66c12108fc68ee0edb4c264ca8392537745
Aaron Southammavong <aaroneight07@gmail.com>
2020-03-04 15:50:45 -0800

    completes characterFrequency.js

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -35,7 +35,49 @@
  *
  */
 
-
 var characterFrequency = function(string) {
-  return result;
+  let letterObj = {};
+  let letterArray = [];
+
+  // edge case for an empty string/falsy value
+  if (!string) {
+    return [];
+  }
+
+  // we want to iterate over each character, so let's turn the string into an array of individual characters
+  let stringArray = string.split('');
+
+  // iterate over the string array
+  // push each letter as a key into an object for storage
+  // track the number of occurrences of each letter
+  stringArray.forEach(letter => {
+    letterObj[letter] = letterObj[letter] + 1 || 1;
+  });
+  // console.log(letterObj);
+
+  // iterate over the storage object
+  // push each key:value pair into the letter array with the letter as the first index and occurrence as the second
+  for (let key in letterObj) {
+    letterArray.push([key, letterObj[key]]);
+  }
+  // console.log(letterArray);
+
+  // sort the array from highest number of occurrences to lowest
+  sortedArray = letterArray.sort(function(a, b) {
+    if (a[1] > b[1]) {
+      return -1;
+    } else if (a[1] < b[1]) {
+      return 1;
+    } else {
+      if (a[0] > b[0]) {
+        return 1;
+      } else if (a[0] < b[0]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    }
+  });
+
+  return sortedArray;
 };
